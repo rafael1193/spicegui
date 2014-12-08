9c22159f902630072884f6a7cac175251cc2ee7b
Rafael Bailón <rafaelbailon@ieee.org>
2014-12-08 20:53:02 +0100

    Use header bar on RunningDialog if it is supported

--- spicegui/running_dialog.py
+++ spicegui/running_dialog.py
@@ -5,30 +5,49 @@ from gi.repository import Gtk, GObject
 class RunningDialog(Gtk.Dialog):
 
     def __init__(self, parent, event):
-        Gtk.Dialog.__init__(self, "Simulation", parent, Gtk.DialogFlags.MODAL|Gtk.DialogFlags.DESTROY_WITH_PARENT|Gtk.DialogFlags.USE_HEADER_BAR,
-            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
-
+        if Gtk.check_version(3, 12, 0) is None: # Use header bar
+            Gtk.Dialog.__init__(self, "Simulation", parent, 
+                                Gtk.DialogFlags.MODAL | 
+                                Gtk.DialogFlags.DESTROY_WITH_PARENT,
+                                (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL), 
+                                use_header_bar=True)
+            self.get_header_bar().set_show_close_button(False)
+        else: # Do not use header bar
+            Gtk.Dialog.__init__(self, "Simulation", parent, 
+                    Gtk.DialogFlags.MODAL | 
+                    Gtk.DialogFlags.DESTROY_WITH_PARENT,
+                    (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL))
+        
         self.event = event
         
         self.set_default_size(150, 100)
+        self.props.resizable = False 
         self.set_default_response(Gtk.ResponseType.CANCEL)
 
         self.progress_bar = Gtk.ProgressBar()
         self.progress_bar.activity_mode = True
         self.progress_bar.pulse()
-        self.progress_bar.set_text("Running simulation")
+        self.progress_bar.set_text(u"Running ngspice…")
         self.progress_bar.set_show_text(True)
         
-        self.label = Gtk.Label(label="Running simulation")
-        self.label.set_vexpand(True)
+#        self.label = Gtk.Label(label=u"Running ngspice…")
+#        self.label.set_vexpand(True)
         
-        self.spinner = Gtk.Spinner()
-        self.spinner.start()
-        self.spinner.set_vexpand(True)
+#        self.spinner = Gtk.Spinner()
+#        self.spinner.start()
+#        self.spinner.set_vexpand(True)
         
         self.hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self.hbox.pack_start(self.spinner, True, True, 18)
-        self.hbox.pack_start(self.label, True, True, 18)
+        self.hbox.set_margin_left(18)
+        self.hbox.set_margin_top(18)
+        self.hbox.set_margin_bottom(18)
+        self.hbox.set_margin_right(18)
+        self.hbox.set_vexpand(True)
+        self.hbox.set_spacing(12)
+        
+        self.hbox.pack_start(self.progress_bar, True, True, 18)
+#        self.hbox.pack_start(self.spinner, True, True, 18)
+#        self.hbox.pack_start(self.label, True, True, 18)
         
         
         box = self.get_content_area()
@@ -49,8 +68,10 @@ class RunningDialog(Gtk.Dialog):
 
 
 if __name__ == "__main__":
+    from multiprocessing import Event
+    event_test = Event()
     win = Gtk.Window()
-    dialog = RunningDialog(win)
+    dialog = RunningDialog(win, event_test)
     dialog.run()
     dialog.destroy()
     win.destroy()
