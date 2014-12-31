c82c28b7bf492f6cb41453634f28ccef540a48b5
Rafael Bailón <rafaelbailon@ieee.org>
2014-12-31 01:00:11 +0100

    Find locale folder path

--- spicegui/__init__.py
+++ spicegui/__init__.py
@@ -21,6 +21,32 @@
 
 from __future__ import print_function
 
+import os
+import os.path
+
+
+def get_locale_path():
+    """Get locale folder path.
+    
+    Returns:
+        locale_path: Locale folder path.
+        
+    Raises:
+        IOError: If locale path cannot be found.
+    """
+    local = os.path.join(os.path.dirname(os.path.abspath(__file__)),"locale")
+    print(local)
+    installed_base = os.path.join(" ",*(os.path.dirname(os.path.abspath(__file__)).split(os.sep)[:-4])).lstrip(" ")  # Remove lib/python2.7/site-packages/spicegui
+    installed_ext = os.path.join("share", "locale")
+    installed = os.path.join(installed_base, installed_ext)
+    print(installed)
+
+    if os.path.exists(local):
+        return local
+    elif os.path.exists(installed):
+        return installed
+    else:
+        raise IOError("Locale path not found.")
 
 def start():
     """Starts SpiceGUI application."""
@@ -28,11 +54,11 @@ def start():
     import locale
 
     domain = "messages"
-    locales_path = "./locales"
+    locale_path = get_locale_path()
 
-    gettext.install(domain, locales_path)
+    gettext.install(domain, locale_path)
     locale.textdomain(domain)
-    locale.bindtextdomain(domain, locales_path)
+    locale.bindtextdomain(domain, locale_path)
     locale.textdomain(domain)
     locale.setlocale(locale.LC_ALL, '')
 
